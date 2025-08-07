5fb1ea7b2cfa30d437d2333c7878b3c85419063e
Joey Holtzman <joey.holtzman@praetorian.com>
2025-08-07 09:57:39 -0500

    Support authentication and other container registries

--- pkg/links/docker/download.go
+++ pkg/links/docker/download.go
@@ -3,12 +3,15 @@ package docker
 import (
 	"archive/tar"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
 
@@ -17,11 +20,7 @@ import (
 	"github.com/praetorian-inc/janus-framework/pkg/types"
 )
 
-const (
-	registryBase = "https://registry-1.docker.io"
-	authBase     = "https://auth.docker.io"
-	authService  = "registry.docker.io"
-)
+const defaultRegistryBase = "https://registry-1.docker.io"
 
 type AuthResponse struct {
 	Token string `json:"token"`
@@ -60,7 +59,9 @@ type ManifestEntry struct {
 // and saves them as tar files.
 type DockerDownloadLink struct {
 	*chain.Base
-	outDir string
+	outDir      string
+	token       string
+	dockerImage *types.DockerImage
 }
 
 func NewDockerDownload(configs ...cfg.Config) chain.Link {
@@ -98,6 +99,7 @@ func (dd *DockerDownloadLink) Process(dockerImage *types.DockerImage) error {
 	if dockerImage.Image == "" {
 		return fmt.Errorf("Docker image name is required")
 	}
+	dd.dockerImage = dockerImage
 
 	outFile, err := createOutputFile(dd.outDir, dockerImage.Image)
 	if err != nil {
@@ -105,7 +107,7 @@ func (dd *DockerDownloadLink) Process(dockerImage *types.DockerImage) error {
 	}
 	defer outFile.Close()
 
-	if err := dd.downloadImage(dockerImage.Image, outFile.Name()); err != nil {
+	if err := dd.downloadImage(dockerImage, outFile.Name()); err != nil {
 		return fmt.Errorf("failed to download image %s: %w", dockerImage.Image, err)
 	}
 
@@ -113,20 +115,16 @@ func (dd *DockerDownloadLink) Process(dockerImage *types.DockerImage) error {
 	return dd.Send(dockerImage)
 }
 
-// This is an adapted version of https://github.com/moby/moby/blob/master/contrib/download-frozen-image-v2.sh
-func (dd *DockerDownloadLink) downloadImage(image, outputTarFile string) error {
-	// Default to latest tag if no tag is provided
-	tag := "latest"
-	if strings.Contains(image, ":") {
-		parts := strings.Split(image, ":")
-		image = parts[0]
-		tag = parts[1]
+func (dd *DockerDownloadLink) getRegistryBase(dockerImage *types.DockerImage) string {
+	if dockerImage.AuthConfig.ServerAddress != "" {
+		return dockerImage.AuthConfig.ServerAddress
 	}
+	return defaultRegistryBase
+}
 
-	// Official images need the library prefix
-	if !strings.Contains(image, "/") {
-		image = "library/" + image
-	}
+// This is an adapted version of https://github.com/moby/moby/blob/master/contrib/download-frozen-image-v2.sh
+func (dd *DockerDownloadLink) downloadImage(dockerImage *types.DockerImage, outputTarFile string) error {
+	imageName, tag := dd.parseImageName(dockerImage.Image)
 
 	// Image components will be downloaded here before being added to a tar file
 	tempDir, err := os.MkdirTemp("", "docker-download-*")
@@ -135,12 +133,11 @@ func (dd *DockerDownloadLink) downloadImage(image, outputTarFile string) error {
 	}
 	defer os.RemoveAll(tempDir)
 
-	token, err := dd.getToken(image)
-	if err != nil {
+	if err := dd.refreshToken(dockerImage); err != nil {
 		return err
 	}
 
-	manifestData, err := dd.getManifest(token, image, tag)
+	manifestData, err := dd.getManifest(imageName, tag)
 	if err != nil {
 		return err
 	}
@@ -160,7 +157,7 @@ func (dd *DockerDownloadLink) downloadImage(image, outputTarFile string) error {
 	case 2:
 		switch baseManifest.MediaType {
 		case "application/vnd.oci.image.manifest.v1+json", "application/vnd.docker.distribution.manifest.v2+json":
-			manifestEntry, err = dd.handleSingleManifestV2(manifestData, image, tag, tempDir)
+			manifestEntry, err = dd.handleSingleManifestV2(manifestData, imageName, tag, tempDir)
 			if err != nil {
 				return err
 			}
@@ -173,12 +170,12 @@ func (dd *DockerDownloadLink) downloadImage(image, outputTarFile string) error {
 
 			for _, manifest := range manifestList.Manifests {
 				if manifest.Platform.Architecture == runtime.GOARCH {
-					subManifestData, err := dd.getManifest(token, image, manifest.Digest)
+					subManifestData, err := dd.getManifest(imageName, manifest.Digest)
 					if err != nil {
 						return err
 					}
 
-					manifestEntry, err = dd.handleSingleManifestV2(subManifestData, image, tag, tempDir)
+					manifestEntry, err = dd.handleSingleManifestV2(subManifestData, imageName, tag, tempDir)
 					if err != nil {
 						return err
 					}
@@ -212,13 +209,142 @@ func (dd *DockerDownloadLink) downloadImage(image, outputTarFile string) error {
 	return dd.createTarFile(tempDir, outputTarFile)
 }
 
-func (dd *DockerDownloadLink) getToken(image string) (string, error) {
-	url := fmt.Sprintf("%s/token?service=%s&scope=repository:%s:pull", authBase, authService, image)
-	resp, err := http.Get(url)
+func (dd *DockerDownloadLink) parseImageName(imageWithTag string) (string, string) {
+	// Default to latest tag if no tag is provided
+	tag := "latest"
+	image := imageWithTag
+	if strings.Contains(image, ":") {
+		parts := strings.Split(image, ":")
+		image = parts[0]
+		tag = parts[1]
+	}
+
+	// Official images need the library prefix
+	if !strings.Contains(image, "/") {
+		image = "library/" + image
+	}
+
+	return image, tag
+}
+
+func (dd *DockerDownloadLink) refreshToken(dockerImage *types.DockerImage) error {
+	imageName, tag := dd.parseImageName(dockerImage.Image)
+
+	// Reset the token so we know if a refresh is needed versus auth failure
+	dd.token = ""
+
+	// Always try to access the registry first to get the WWW-Authenticate header or see if a token is required.
+	registryBase := dd.getRegistryBase(dockerImage)
+	url := fmt.Sprintf("%s/v2/%s/manifests/%s", registryBase, imageName, tag)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// We expect a 401 with WWW-Authenticate header to tell us how to authenticate to the registry
+	// if a token is required.
+	if resp.StatusCode == http.StatusUnauthorized {
+		wwwAuth := resp.Header.Get("WWW-Authenticate")
+		if wwwAuth == "" {
+			return fmt.Errorf("401 response without WWW-Authenticate header")
+		}
+
+		authURL, err := dd.parseWWWAuthenticate(wwwAuth, imageName)
+		if err != nil {
+			return fmt.Errorf("failed to parse WWW-Authenticate header: %w", err)
+		}
+
+		if dockerImage.AuthConfig.Username != "" && dockerImage.AuthConfig.Password != "" {
+			token, err := dd.getTokenFromAuthEndpoint(authURL, dockerImage, true)
+			if err != nil {
+				return err
+			}
+
+			dd.token = token
+			return nil
+		}
+
+		// No credentials provided, try anonymous access.
+		token, err := dd.getTokenFromAuthEndpoint(authURL, dockerImage, false)
+		if err != nil {
+			return fmt.Errorf("failed to get token for %s: %w", imageName, err)
+		}
+		dd.token = token
+		return nil
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		// A token is not needed if we can access the manifest.
+		return nil
+	}
+
+	return fmt.Errorf("unexpected status code %d", resp.StatusCode)
+}
+
+// Parse the WWW-Authenticate header and returns the auth endpoint.
+func (dd *DockerDownloadLink) parseWWWAuthenticate(wwwAuth, image string) (string, error) {
+	// The format of the WWW-Authenticate header is from the OAuth 2.0 spec.
+	// Bearer realm="<url>",service="<service>",scope="<scope>"
+
+	// The realm is required to get the auth endpoint.
+	realmMatch := regexp.MustCompile(`realm="([^"]+)"`).FindStringSubmatch(wwwAuth)
+	if len(realmMatch) < 2 {
+		return "", fmt.Errorf("could not parse realm from WWW-Authenticate: %s", wwwAuth)
+	}
+
+	authURL := realmMatch[1]
+
+	// The service and scope are optional.
+	serviceMatch := regexp.MustCompile(`service="([^"]+)"`).FindStringSubmatch(wwwAuth)
+	scopeMatch := regexp.MustCompile(`scope="([^"]+)"`).FindStringSubmatch(wwwAuth)
+
+	params := make([]string, 0)
+	if len(serviceMatch) >= 2 {
+		params = append(params, fmt.Sprintf("service=%s", serviceMatch[1]))
+	}
+	if len(scopeMatch) >= 2 {
+		params = append(params, fmt.Sprintf("scope=%s", scopeMatch[1]))
+	} else {
+		// Default scope
+		params = append(params, fmt.Sprintf("scope=repository:%s:pull", image))
+	}
+
+	if len(params) > 0 {
+		authURL += "?" + strings.Join(params, "&")
+	}
+
+	return authURL, nil
+}
+
+func (dd *DockerDownloadLink) getTokenFromAuthEndpoint(authURL string, dockerImage *types.DockerImage, withAuth bool) (string, error) {
+	req, err := http.NewRequest("GET", authURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// Add authentication if requested
+	if withAuth && dockerImage.AuthConfig.Username != "" && dockerImage.AuthConfig.Password != "" {
+		auth := fmt.Sprintf("%s:%s", dockerImage.AuthConfig.Username, dockerImage.AuthConfig.Password)
+		encodedAuth := base64.StdEncoding.EncodeToString([]byte(auth))
+		req.Header.Set("Authorization", "Basic "+encodedAuth)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("HTTP %d: failed to get token from auth endpoint", resp.StatusCode)
+	}
 
 	var authResp AuthResponse
 	if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
@@ -228,16 +354,60 @@ func (dd *DockerDownloadLink) getToken(image string) (string, error) {
 	return authResp.Token, nil
 }
 
-func (dd *DockerDownloadLink) fetchBlob(token, image, digest, targetFile string) error {
+// doRequestWithRetry performs an HTTP request with automatic token refresh and retry logic.
+// If a 401 is received, it attempts to refresh the token and retry once.
+// If the retry also fails with 401, it returns an auth error.
+func (dd *DockerDownloadLink) doRequestWithRetry(req *http.Request) (*http.Response, error) {
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// If we get a 401, try to refresh the token and retry once
+	if resp.StatusCode == http.StatusUnauthorized {
+		resp.Body.Close() // Close the first response
+
+		slog.Debug("unauthorized, attempting to refresh token")
+		if err := dd.refreshToken(dd.dockerImage); err != nil {
+			return nil, fmt.Errorf("failed to refresh token: %w", err)
+		}
+
+		// Update the request with the new token
+		if dd.token != "" {
+			req.Header.Set("Authorization", "Bearer "+dd.token)
+		}
+
+		// Retry the request
+		resp2, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		// If we still get 401 after refresh, it's an auth issue
+		if resp2.StatusCode == http.StatusUnauthorized {
+			resp2.Body.Close()
+			return nil, fmt.Errorf("insufficient permissions to access private repository - credentials required")
+		}
+
+		return resp2, nil
+	}
+
+	return resp, nil
+}
+
+func (dd *DockerDownloadLink) fetchBlob(image, digest, targetFile string) error {
+	registryBase := dd.getRegistryBase(dd.dockerImage)
 	url := fmt.Sprintf("%s/v2/%s/blobs/%s", registryBase, image, digest)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Authorization", "Bearer "+token)
+	if dd.token != "" {
+		req.Header.Set("Authorization", "Bearer "+dd.token)
+	}
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := dd.doRequestWithRetry(req)
 	if err != nil {
 		return err
 	}
@@ -258,6 +428,10 @@ func (dd *DockerDownloadLink) fetchBlob(token, image, digest, targetFile string)
 		resp = resp2
 	}
 
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP %d: failed to fetch blob", resp.StatusCode)
+	}
+
 	file, err := os.Create(targetFile)
 	if err != nil {
 		return err
@@ -268,7 +442,8 @@ func (dd *DockerDownloadLink) fetchBlob(token, image, digest, targetFile string)
 	return err
 }
 
-func (dd *DockerDownloadLink) getManifest(token, image, digest string) ([]byte, error) {
+func (dd *DockerDownloadLink) getManifest(image, digest string) ([]byte, error) {
+	registryBase := dd.getRegistryBase(dd.dockerImage)
 	url := fmt.Sprintf("%s/v2/%s/manifests/%s", registryBase, image, digest)
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -276,14 +451,16 @@ func (dd *DockerDownloadLink) getManifest(token, image, digest string) ([]byte, 
 		return nil, err
 	}
 
-	req.Header.Set("Authorization", "Bearer "+token)
+	if dd.token != "" {
+		req.Header.Set("Authorization", "Bearer "+dd.token)
+	}
 	req.Header.Set("Accept", "application/vnd.oci.image.manifest.v1+json")
 	req.Header.Add("Accept", "application/vnd.oci.image.index.v1+json")
 	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
 	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.list.v2+json")
 	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v1+json")
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := dd.doRequestWithRetry(req)
 	if err != nil {
 		return nil, err
 	}
@@ -307,17 +484,12 @@ func (dd *DockerDownloadLink) handleSingleManifestV2(manifestJson []byte, image,
 		return ManifestEntry{}, err
 	}
 
-	token, err := dd.getToken(image)
-	if err != nil {
-		return ManifestEntry{}, err
-	}
-
 	// Download config
 	configDigest := manifest.Config.Digest
 	imageId := strings.TrimPrefix(configDigest, "sha256:")
 	configFile := imageId + ".json"
 
-	if err := dd.fetchBlob(token, image, configDigest, filepath.Join(outputDir, configFile)); err != nil {
+	if err := dd.fetchBlob(image, configDigest, filepath.Join(outputDir, configFile)); err != nil {
 		return ManifestEntry{}, err
 	}
 
@@ -348,7 +520,7 @@ func (dd *DockerDownloadLink) handleSingleManifestV2(manifestJson []byte, image,
 				continue
 			}
 
-			if err := dd.fetchBlob(token, image, layer.Digest, layerPath); err != nil {
+			if err := dd.fetchBlob(image, layer.Digest, layerPath); err != nil {
 				return ManifestEntry{}, err
 			}
 
